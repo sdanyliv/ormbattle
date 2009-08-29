3b9c2a3139c0be2cdb3ea10c72cd706d672a3ab5
alex.yakunin <alex.yakunin@localhost>
2009-08-29 09:49:15 +0000

    Bugfix in .bat.

--- Run-DO.bat
+++ Run-DO.bat
@@ -1,4 +1,4 @@
 @echo off
 pushd TestRunner\bin\Release
-  start /WAIT cmd /C ..\..\..\Redirect.bat ..\..\..\Output-DO.txt TestRunner.exe -t:DO,EF,NH
+  start /WAIT cmd /C ..\..\..\Redirect.bat ..\..\..\Output-DO.txt TestRunner.exe "-t:DO,EF,NH"
 popd
