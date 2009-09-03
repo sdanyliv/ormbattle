b2abbf7ef634a898105f89b1a3690ac909d0cb02
alex.yakunin <alex.yakunin@localhost>
2009-09-03 14:21:39 +0000

    BLToolkit is included into official tests (Run-Battle.bat).

--- Run-Battle.bat
+++ Run-Battle.bat
@@ -1,4 +1,4 @@
 @echo off
 pushd TestRunner\bin\Release
-  start /HIGH /WAIT cmd /C ..\..\..\Redirect.bat ..\..\..\Output.txt TestRunner.exe "-t:SQL,EF,LS,NH,OA,SS"
+  start /HIGH /WAIT cmd /C ..\..\..\Redirect.bat ..\..\..\Output.txt TestRunner.exe "-t:SQL,EF,BLT,LS,NH,OA,SS"
 popd
