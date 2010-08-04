561cf358b190c99a0f469f7227693a65b3f027de
Alex Yakunin <Alex Yakunin@localhost>
2010-08-04 23:43:17 +0600

    Minor improvements.

--- Run-LINQ.bat
+++ Run-LINQ.bat
@@ -1,4 +1,7 @@
 @echo off
+set ReportsPath=..\..\..\_Reports
+set Output=Output-LINQ
 pushd TestRunner\bin\Release
-  start /HIGH /WAIT cmd /C ..\..\..\Redirect.bat ..\..\..\Output-LINQ.txt TestRunner.exe "-pt:" "-luc" "-json:..\..\..\Output-LINQ.js"
+  start /HIGH /WAIT cmd /C ..\..\..\Redirect.bat %ReportsPath%\%Output%.txt TestRunner.exe "-pt:" "-luc" "-json:%ReportsPath%\json\%Output%.js"
 popd
+copy /Y "%ReportsPath%\%Output%.txt" .
