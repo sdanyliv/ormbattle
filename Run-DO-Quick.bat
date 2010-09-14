6ad2059ba5e2b7c9752bfaadc9f976d408e03275
Alex Yakunin <Alex Yakunin@localhost>
2010-09-15 02:27:48 +0600

    Minor fix.

--- Run-DO-Quick.bat
+++ Run-DO-Quick.bat
@@ -1,6 +1,6 @@
 @echo off
 set ReportsPath=_Reports
-set Output=Output-DO
+set Output=Output-DO-Quick
 pushd TestRunner\bin\Release
   set RootPath=..\..\..
   start /HIGH /WAIT cmd /C %RootPath%\Redirect.bat %RootPath%\%ReportsPath%\%Output%.txt TestRunner.exe "-t:DO" "-ppc:3" "-pi:1000,5000" "-json:%RootPath%\%ReportsPath%\json\%Output%.json"
